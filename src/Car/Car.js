c7d8cdcd56655856e1b4b9ea196dd2c3fa9d56f7
zhukoff-av <mr.zhukoff@gmail.com>
2022-11-18 13:32:01 +0100

    test state functions

---
+++ src/Car/Car.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default props => (
+    <div style={{
+        border: '1px solid #ccc',
+        marginBottom: '10px',
+        display: 'block',
+        padding: '10px'
+    }}
+    >
+        <h3>Сar name: {props.name}</h3>
+        <p>Year: <strong>{props.year}</strong></p>
+        <input type="text" onChange={props.onChangeName} value={props.name}/>
+        <button onClick={props.onDelete}>Delete</button>
+    </div>
+)
