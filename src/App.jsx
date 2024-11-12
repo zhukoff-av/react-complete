7a1de51cd48e12b810289234442be092dffec736
Anton Zhukov <anton.zhukov@immowelt.de>
2024-11-12 17:11:20 +0100

    add new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SearchParams from "./SearchParams";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import ContentPage from "./ContentPage";
+import React from "react";
 
 const App = () => {
-  return (<div>
-    <h1>Hey Hey </h1>
-    <SearchParams />
-  </div>)
+  return (
+      <Router>
+        <div>
+          <h1 style={{ color: 'black' }}>Hey Hey Hey HeyHey </h1>
+          <Routes>
+            <Route path="/" element={<SearchParams />} />
+            <Route path="/content" element={<ContentPage />} />
+          </Routes>
+        </div>
+      </Router>
+  );
 };
 
 const container = document.getElementById("root");
---
+++ src/ContentPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContentPage = () => {
+    return (
+        <div>
+            <h1>Content Page</h1>
+            <p>This is some content displayed on the new page.</p>
+        </div>
+    );
+};
+
+export default ContentPage;
