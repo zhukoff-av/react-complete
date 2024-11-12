3d9a270456c6ac862e2003c4596d1978ffa1a999
zhukoff-av <mr.zhukoff@gmail.com>
2024-11-12 20:55:40 +0100

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-import { createRoot } from "react-dom/client";
-import SearchParams from "./SearchParams";
+import {createRoot} from "react-dom/client";
 import React from "react";
+import ImageSlider from "./ImageSlider";
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
 
 const App = () => {
-  return (<div>
-    <h1>Hey Hey </h1>
-    <SearchParams />
-  </div>)
+
+    const images = [
+        "/img/image1.jpg",
+        "/img/image2.jpg",
+        "/img/image3.jpg",
+    ]
+
+    return (
+        <div>
+            <header className={"relative flex flex-col items-center"}>
+                <h2>Header</h2>
+            </header>
+            <ImageSlider images={images}/>
+
+            <footer className={"relative flex flex-col items-center"}>
+                <h2>Footer</h2>
+            </footer>
+        </div>
+    )
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(<App/>);
